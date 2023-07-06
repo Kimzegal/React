b38626ad7c0469de13283ee39192b2c422b75aae
kimzegal <asdskylee@gmail.com>
2023-07-07 01:03:19 +0900

    반복문 복습

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav, Navbar, Container, Row, Col} from 'react-bootstrap';
+import "./App.css";
+import data from "./data.js";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, Navbar, Container, Row, Col } from "react-bootstrap";
 
 function App() {
+  let [songs] = useState(data);
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -20,25 +23,25 @@ function App() {
 
       <Container fluid>
         <Row>
-          <Col md={4}>
-          <img src="/img/001.jpg" alt="아틀란티스 소녀" width="100%"/>
-            <h4>아틀란티스 소녀</h4>
-            <p>BoA</p>
-          </Col>
-          <Col md={4}>
-          <img src="/img/002.jpg" alt="more than words" width="100%"/>
-            <h4>More than words</h4>
-            <p>5tion</p>
-          </Col>
-          <Col md={4}>
-          <img src="/img/003.jpg" alt="안되나요" width="100%"/>
-            <h4>안되나요...</h4>
-            <p>휘성</p>
-          </Col>
+          {songs.map((a, i) => {
+            return <Post sing={songs[i]} num={i + 1}></Post>;
+          })}
         </Row>
       </Container>
-    
     </div>
+  );
+}
+
+function Post(props) {
+  console.log(props);
+  return (
+    <>
+      <Col md={4}>
+        <img src={props.sing.img} alt="아틀란티스 소녀" width="100%" />
+        <h4>{props.sing.title}</h4>
+        <p>{props.sing.singer}</p>
+      </Col>
+    </>
   );
 }
 
---
+++ shop/src/data.js
@@ -0,0 +1,22 @@
+let data = [
+  {
+    id: 1,
+    title: "아틀란티스 소녀",
+    singer: "BoA",
+    img: "/img/001.jpg",
+  },
+  {
+    id: 2,
+    title: "More than words",
+    singer: "5tion",
+    img: "/img/002.jpg",
+  },
+  {
+    id: 3,
+    title: "안되나요...",
+    singer: "휘성",
+    img: "/img/003.jpg",
+  },
+];
+
+export default data;
